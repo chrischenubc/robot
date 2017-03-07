59379b505a6370eddd979b5d668ef6671a5c5a9a
Ziqiao John Lin <linziqiao0620@gmail.com>
2017-03-07 01:07:29 -0800

    Add files via upload
    hall-effect-another method from online https://forum.arduino.cc/index.php?topic=425010.0

---
+++ hall-effect/hall-effect-another_method.ino
@@ -0,0 +1,63 @@
+volatile byte  count = 0;
+byte numCount = 8; //number of pulse intervals to measure
+  
+
+volatile unsigned long startTime;
+volatile unsigned long endTime;
+unsigned long copy_startTime;
+unsigned long copy_endTime;
+
+volatile boolean finishCount = false;
+float period;
+
+unsigned int rpm = 0;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("start...");
+
+  attachInterrupt(digitalPinToInterrupt(3), isrCount, FALLING);//interrupt on pin3
+}
+
+void loop()
+{
+  if (finishCount == true)
+  {
+    finishCount = false;//reset flag
+    // disable interrupts, make protected copy of time values
+    noInterrupts();
+    copy_startTime = startTime;
+    copy_endTime = endTime;
+    count = 0;
+    interrupts();
+
+    period = (copy_endTime - copy_startTime) / 1000.0; //micros to millis
+    //debug prints
+    Serial.print(period); //total time for numCount
+    Serial.print('\t');
+    Serial.println(period/numCount);//time between individual pulses
+    
+    rpm = numCount * 60.0 * (1000.0 / period);//one count per revolution
+    //rpm = numCount * 30.0 * (1000.0 / period);//two counts per revolution
+  
+    Serial.print("RPM = ");
+    Serial.println(rpm);
+  }
+}
+
+
+void isrCount()
+{
+  if (count == 0)//first entry to isr
+  {
+    startTime = micros();
+  }
+
+  if (count == numCount)
+  {
+    endTime = micros();
+    finishCount = true;    
+  }
+  count++; //increment after test for numCount
+}
