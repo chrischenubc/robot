c2b794d6eb091f869f074e33d068462fade55d21
Yu Chen <chrischenubc@gmail.com>
2017-03-07 22:27:20 -0800

    update

--- code_for_wed/code_for_wed.ino
+++ code_for_wed/code_for_wed.ino
@@ -11,29 +11,43 @@ const int E2 = 6;
 const int M2 = 7;
 
 //pin configurations
-const int TRIG_PIN = 12;
-const int ECHO_PIN = 11;
+const int TRIG_PIN = 11;
+const int ECHO_PIN = 12;
 const int SERVO_PIN = 13;
 const int LM_PIN = A0;      //Pin for LM-35 temperature sensor
-const int SWITCH_PIN = 3;        //Pin for pull up Switch
+const int SWITCH_PIN = 8;        //Pin for pull up Switch
 
 //define some constants here
 const int DISTANCE_LIMIT = 25;
 const int MAX_SPEED = 255;
 const int DECELERATION = 10;
 
+// main switch pin configuration
+const int MAIN_SWITCH_PIN = A5;
+
+
 //define some global variables here
 int flag=1;
 char instr='1';
 int dis;
-
+int main_switch = 0; 
 //declaration of variables for hall effect sensor
 volatile byte half_revolutions;
 unsigned int rpm;
 unsigned long timeold;
 
+float speedLeft;
+float speedRight;
+long normalPower=200;
+long newPower=normalPower;
 
 //Declaration of variables for basic function 2
+//Pins for 2 optical reflective sensors
+const int ORsensorpin_1 = A1;
+const int ORsensorpin_2 = A2;
+const int ORsensorpin_3 = A3;
+const int ORsensorpin_4 = A4;
+
 const int LINE_FOLLOW_SPEED = 190;
 const int TURNING_SPEED_NORM = 120;
 const int TURNING_SPEED_DIFF = 80;
@@ -49,8 +63,6 @@ int ORsensorVal_2;
 int ORsensorVal_3;
 int ORsensorVal_4;
 
-
-
 void setup()
 {
   pinMode(M1, OUTPUT);
@@ -59,8 +71,8 @@ void setup()
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
  
-  pinMode(SWITCH_PIN, INPUT);
-
+  pinMode(SWITCH_PIN, INPUT_PULLUP);
+  pinMode(MAIN_SWITCH_PIN,INPUT);
   servo.attach(SERVO_PIN);
   Serial.begin(9600);
   BT.begin(9600);
@@ -74,14 +86,17 @@ void setup()
 
 void loop()
 {    
+  main_switch = digitalRead(MAIN_SWITCH_PIN);
+  if(main_switch == HIGH){    
    /* choose the mode based on the flag we have */    
-  chooseMode(instr);
+   chooseMode(instr);
   /* update the flag based on the signal received */
-  //update_flag(instr);
+  
   /* after successfully change the mode, make it stop and wait for 1 sec */
-  Serial.print("Successfully changed the mode ");
-  Stop();
-  delay(1000); 
+   Serial.print("Successfully changed the mode ");
+   Stop();
+   delay(1000); 
+  }
 }
 
 /*
@@ -96,6 +111,9 @@ void mode1(){
   Serial.println("In mode 1");
   moveForward(255);
   do {
+
+    compareRpm();
+    
     Serial.println("moving forward");
 
     // update_keyboard();
@@ -104,11 +122,11 @@ void mode1(){
      * If there is a new signal, then update the instr
      * and exit the current mode.
      */
-    if(digitalRead(SWITCH_PIN==LOW)){
+    if(digitalRead(SWITCH_PIN)==LOW){
       instr='2';
       return;
     }
-    else if(flag!=1 || instr =='2' || instr =='3' ) {
+    else if(instr!='1'|| instr =='2' || instr =='3' || main_switch ==LOW ) {
       return; //check the interrupt flag
     }
     
@@ -120,7 +138,7 @@ void mode1(){
      break;
     }
      dis=readSonar();
-  }while(dis >= DISTANCE_LIMIT || dis ==-1);
+  }while(dis >= DISTANCE_LIMIT || dis ==-1 && main_switch == HIGH);
 
     /* To detect whether the new signal has arrived
      * If there is a new signal, then update the instr
@@ -128,11 +146,11 @@ void mode1(){
      */
    // update_keyboard();
     update_instr();
-    if(digitalRead(SWITCH_PIN==LOW)){
+    if(digitalRead(SWITCH_PIN)==LOW){
       instr='2';
       return;
     }
-    if(flag!=1 || instr =='2' || instr =='3' ) {
+    if(instr!='1' || instr =='2' || instr =='3'  || main_switch ==LOW) {
       return; //check the interrupt flag
     }
   int scanVal=scanAround(2);
@@ -143,27 +161,23 @@ void mode1(){
 
 //basic functionality 2
 void mode2(){
-
+  lineFollowPrepare();
+  delay(1000);
   while(flag==2) {
     Serial.println("In mode 2");
    // update_keyboard(); 
     update_instr();
-    if(digitalRead(SWITCH_PIN==HIGH)){
+    if(digitalRead(SWITCH_PIN) ==HIGH){
       instr='1';
       return;
     }
-    else if(flag!=2 || instr =='1' || instr =='3' ) {
+    else if(instr!='2' || instr =='1' || instr =='3' || main_switch ==LOW) {
       return;
     }
-
-    /**
-     * TODO:do someting here: codes about function 2
-     */
-       
+    lineFollow();      
   }
  
 }
-
 
 
 
@@ -174,7 +188,7 @@ void mode3(){
     read_instruction();
    // read_keyboard(); 
     //update_keyboard();
-    if(flag!=3 || instr =='1' || instr =='2' ) {
+    if(flag!=3 || instr =='1' || instr =='2'|| main_switch ==LOW ) {
       return;
     }   
   }  
@@ -199,20 +213,16 @@ void chooseMode(char instr){
   }
 }
 
-/*
-void update_flag(char instr){
-  if(instr=='1'){
-        flag=1;
-      }
-  else if (instr=='2'){
-        flag=2;
-  }
-  else if (instr=='3'){
-        flag=3;
-  }
-      
-}*/
 
+/*
+ * The function is to move robot forward at the given power
+ * and the robot will not stop moving forward until it receives a new instruction,such as Stop()
+ * @param power to control the speed
+ * the higher the power is, the fast the robot moves
+ * the power is range from [0,255]
+ * 
+ * 
+ */
 void moveForward(long power) {
   digitalWrite(M1, HIGH);
   digitalWrite(M2, HIGH);
@@ -222,13 +232,14 @@ void moveForward(long power) {
 
 
 /**
- * make the robot stop immediately
+ * The function is to stop the robot immedietly
  */
 void Stop() {
   digitalWrite(M1, HIGH);
   digitalWrite(M2, HIGH);
   analogWrite(E1, 0);
   analogWrite(E2, 0);
+  newPower=normalPower;
 }
 
 /**
@@ -243,8 +254,10 @@ void decelerate(int deceler) {
     analogWrite(E1, i);
     analogWrite(E2, i);
     delay(20);
+    newPower=normalPower;
   }
 }
+
 
 void turnRight(long Time) {
   unsigned long oldtime;
@@ -282,14 +295,7 @@ void turnLeft(long Time) {
   int maxDegree=0;
   servo.write(0); //turn the servo to 0 degree
   for(int i=0;i<=parts;i++){
-
-   /*  update_keyboard(); 
-    if(flag!=1 || (instr=BT.read()) =='B' || instr =='M' ) {
-      switch(instr){
-        case 'M':flag=3;
-      }
-      return -1; //check the interrupt flag
-    }*/
+    
     servo.write(i*180/parts);
     vals[i]=readSonar();
     delay(500);
@@ -371,7 +377,12 @@ int readSwitch(){
 
 /****************************************************************************
 /*
- * This method is to use Bluetooth to control the car
+ * This method is to use Bluetooth to control the car. We will receive the char type 
+ * input from android app via bluetooth, which we named it as "instruction". Then we use
+ * several if statments to determine the command that we receive. 'R' means turn right. 'L'
+ * means turn left. 'M' means move forward. And 'S' means sudden stop. 'D' means deacceleration.
+ * Besides, we also have '1','2' and '3' represents 3 different models, Auto represents '1',
+ * '2' means Tracing model, '3' means Manual mode. If we receive the signal except these, it will return error 
  */
 void read_instruction(){
     char instruction;
@@ -400,24 +411,35 @@ void read_instruction(){
         Stop();
         Serial.println("Sudden Stop");
       }
-      else if(instruction == '1'){     //switch mode to automatical driving
-         flag=1;
+      else if(instruction == '1'){     //switch mode to automatical driving     
          instr='1';
       }
-      else if(instruction == '2'){     //switch mode to automatical driving
-         flag=2;
+      else if(instruction == '2'){     //switch mode to automatical driving      
          instr='2';          
       }
-      else if(instruction == '3'){     //switch mode to automatical driving
-         flag=3;
+      else if(instruction == '3'){     //switch mode to automatical driving       
          instr='3';           
       }       
       else{                            //instruction do not exist
-         Serial.println("Wrong Instruction");         
+         Serial.println("Wrong Instruction");   // print out wrong instruction
       }
     }
 }
 
+
+void update_instr() {
+  if(BT.available()) {
+     String s;
+      s = BT.readString();
+      instr=s[0];
+      Serial.print("keyboard command received:    ");
+      Serial.println(instr);
+    } 
+}
+
+
+
+/****************************************************************************
 
 /*
  * THIS FUNCTION IS FOR TESTING USE
@@ -452,16 +474,13 @@ void read_keyboard(){
         Stop();
         Serial.println("Sudden Stop");
       }
-      else if(instruction == '1'){     //switch mode to automatical driving
-         flag=1;
+      else if(instruction == '1'){     //switch mode to automatical driving        
          instr='1';
       }
-      else if(instruction == '2'){     //switch mode to automatical driving
-         flag=2;
+      else if(instruction == '2'){     //switch mode to automatical driving       
          instr='2';          
       }
       else if(instruction == '3'){     //switch mode to automatical driving
-         flag=3;
          instr='3';           
       }       
       else{                            //instruction do not exist
@@ -470,14 +489,161 @@ void read_keyboard(){
     }
 }
 
-void update_instr() {
-  if(BT.available()) {
-     String s;
-      s = BT.readString();
-      instr=s[0];
-      Serial.print("keyboard command received:    ");
-      Serial.println(instr);
-    } 
+
+//Additional moving functions for basic function 2
+void turnLeftForward_Slow(int power, long Time) {
+  unsigned long oldtime;
+  oldtime = millis();
+
+  while (millis() - oldtime <= Time) {
+    digitalWrite(M1, HIGH);
+    digitalWrite(M2, HIGH);
+    analogWrite(E1, power);
+    analogWrite(E2, power - TURNING_SPEED_DIFF);
+
+    Serial.println("Left");
+
+  scanGround();
+
+  //If the right sensors(1, 2) start to give higher values, then it means the car is moving too left,
+  //so turnleft function should break and stop the robot from moving left
+    if( (ORsensorVal_1 + ORsensorVal_2)/2 >= ((ORsensorVal_3 + ORsensorVal_4)/2 ) &&
+        !(ORsensorVal_1 < midVal - 50 &&  ORsensorVal_2 < midVal - 50 &&
+          ORsensorVal_3 < midVal - 50  && ORsensorVal_4 < midVal - 50 ) ){
+      break;
+    }
+  }
+}
+
+void turnLeftForward_Fast(int power, long Time) {
+  unsigned long oldtime;
+  oldtime = millis();
+
+  while (millis() - oldtime <= Time) {
+    digitalWrite(M1, HIGH);
+    digitalWrite(M2, HIGH);
+    analogWrite(E1, power);
+    analogWrite(E2, 0);
+
+    Serial.println("FasterLeft");
+
+  scanGround();
+    
+  //If the right sensors(1, 2) start to give higher values, then it means the car is moving too left,
+  //so turnleft function should break and stop the robot from moving left
+    if( (ORsensorVal_1 + ORsensorVal_2)/2 >= ((ORsensorVal_3 + ORsensorVal_4)/2 ) && 
+        !(ORsensorVal_1 < midVal - 50 &&  ORsensorVal_2 < midVal - 50 &&
+          ORsensorVal_3 < midVal - 50  && ORsensorVal_4 < midVal - 50 ) ){
+      break;
+    }
+  }
+}
+
+void turnRightForward_Slow(int power, long Time) {
+  unsigned long oldtime;
+  oldtime = millis();
+
+  while (millis() - oldtime <= Time) {
+    digitalWrite(M1, HIGH);
+    digitalWrite(M2, HIGH);
+    analogWrite(E1, power - TURNING_SPEED_DIFF);
+    analogWrite(E2, power);
+
+  scanGround();
+
+  //If the left sensors(3, 4) start to give higher values, then it means the car is moving too right,
+  //so turnright function should break and stop the robot from moving right
+    if( (ORsensorVal_1 + ORsensorVal_2)/2 <= ((ORsensorVal_3 + ORsensorVal_4)/2) && 
+         !(ORsensorVal_1 < midVal - 50 &&  ORsensorVal_2 < midVal - 50 &&
+          ORsensorVal_3 < midVal - 50  && ORsensorVal_4 < midVal - 50 ) ){
+      break;
+    }
+  }
+}
+
+void turnRightForward_Fast(int power, long Time) {
+  unsigned long oldtime;
+  oldtime = millis();
+
+  while (millis() - oldtime <= Time) {
+    digitalWrite(M1, HIGH);
+    digitalWrite(M2, HIGH);
+    analogWrite(E1, 0);
+    analogWrite(E2, power);
+
+  scanGround();
+
+  //If the left sensors(3, 4) start to give higher values, then it means the car is moving too right,
+  //so turnright function should break and stop the robot from moving right
+    if( (ORsensorVal_1 + ORsensorVal_2)/2 <= ((ORsensorVal_3 + ORsensorVal_4)/2) && 
+         !(ORsensorVal_1 < midVal - 50 &&  ORsensorVal_2 < midVal - 50 &&
+          ORsensorVal_3 < midVal - 50  && ORsensorVal_4 < midVal - 50 ) ){
+      break;
+    }
+  }
+}
+
+
+void sharpTurnLeft(int power, long Time) {
+  unsigned long oldtime;
+  oldtime = millis();
+
+  while (millis() - oldtime <= Time) {
+    digitalWrite(M1, HIGH);
+    digitalWrite(M2, LOW);
+    analogWrite(E1, power);
+    analogWrite(E2, power);
+
+  scanGround();
+
+  //If the right sensors(1, 2) start to give higher values, then it means the car is moving too left,
+  //so turnright function should break and stop the robot from moving left
+    if( (ORsensorVal_1 + ORsensorVal_2)/2 >= ((ORsensorVal_3 + ORsensorVal_4)/2) && 
+         !(ORsensorVal_1 < midVal - 50 &&  ORsensorVal_2 < midVal - 50 &&
+          ORsensorVal_3 < midVal - 50  && ORsensorVal_4 < midVal + 50 ) ){
+      break;
+    }
+  }
+}
+
+void sharpTurnRight(int power, long Time) {
+  unsigned long oldtime;
+  oldtime = millis();
+
+  while (millis() - oldtime <= Time) {
+    digitalWrite(M1, LOW);
+    digitalWrite(M2, HIGH);
+    analogWrite(E1, power);
+    analogWrite(E2, power);
+    
+  scanGround();
+
+  //If the left sensors(1, 2) start to give higher values, then it means the car is moving too right,
+  //so turnright function should break and stop the robot from moving right
+    if( (ORsensorVal_1 + ORsensorVal_2)/2 >= ((ORsensorVal_3 + ORsensorVal_4)/2) && 
+         !(ORsensorVal_1 < midVal - 50 &&  ORsensorVal_2 < midVal - 50 &&
+          ORsensorVal_3 < midVal - 50  && ORsensorVal_4 < midVal - 50 ) ){
+      break;
+    }
+  }
+}
+
+boolean checkBreakpoint(long Time) {
+  unsigned long oldtime;
+  oldtime = millis();
+
+  while(millis() - oldtime < Time){
+    moveForward(LINE_FOLLOW_SPEED);
+
+    scanGround();
+  
+    if(!(ORsensorVal_1 < midVal - 50 &&  ORsensorVal_2 < midVal - 50 &&
+     ORsensorVal_3 < midVal - 50  && ORsensorVal_4 < midVal - 50) ){
+      return true;
+    }
+  }
+
+  return false;
 }
 
 
@@ -494,5 +660,138 @@ void update_keyboard() {
       Serial.println(instr);
      }
 }
+
+
+/*
+ * The function is to move robot forward at the given power
+ * and the robot will not stop moving forward until it receives a new instruction,such as Stop()
+ * @param power to control the speed
+ * the higher the power is, the fast the robot moves
+ * the power is range from [0,255]
+ * 
+ * 
+ */
+void adjustMovSpeed(long leftPower, long rightPower) {
+  digitalWrite(M1, HIGH);
+  digitalWrite(M2, HIGH);
+  analogWrite(E1, leftPower);
+  analogWrite(E2, rightPower);
+}
+
+void compareRpm(){
+  
+  if(speedLeft>speedRight){
+    adjustMovSpeed(newPower-5,normalPower);
+  }
+  else if(speedLeft<speedRight){
+    adjustMovSpeed(newPower+5,normalPower);
+  }
+}
+
+char lineFollow(){
+  int turningTime = 1000;
+
+  scanGround();
+  /*Serial.print(midVal);
+  Serial.println("wtf");
+
+  Serial.println(ORsensorVal_1);
+  Serial.println(ORsensorVal_2);
+  Serial.println(ORsensorVal_3);
+  Serial.println(ORsensorVal_4);*/
+
+  //delay(500);
+
+  //If all the values are lower than midVal, it means the robot is on the surface
+  if(!(ORsensorVal_1 < midVal - 50 &&  ORsensorVal_2 < midVal - 50 &&
+     ORsensorVal_3 < midVal - 50  && ORsensorVal_4 < midVal - 50 )){
+
+    //If the difference between left and right side sensors are lower than 200, go forward
+    if( ((ORsensorVal_1 + ORsensorVal_2)/2 <= ((ORsensorVal_3 + ORsensorVal_4)/2 + 200)) &&
+        ((ORsensorVal_1 + ORsensorVal_2)/2 >= ((ORsensorVal_3 + ORsensorVal_4)/2 - 200)) ){
+          moveForward(LINE_FOLLOW_SPEED);
+    }else if(ORsensorVal_1 < midVal && ORsensorVal_2 < midVal &&
+       ORsensorVal_3 < midVal && ORsensorVal_4 < midVal){
+        moveForward(LINE_FOLLOW_SPEED);  
+    }
+
+  //Sharp turn
+    if(ORsensorVal_1 > midVal + 100 &&  ORsensorVal_2 > midVal + 100 && ORsensorVal_3 > midVal + 100 && ORsensorVal_4 < midVal - 100 ){
+      sharpTurnRight(160, turningTime);
+    }else if(ORsensorVal_1 < midVal - 100 && ORsensorVal_2 > midVal + 100 &&  ORsensorVal_3 > midVal + 100 && ORsensorVal_4 > midVal + 100){
+      sharpTurnLeft(160, turningTime);
+    }    
+    
+  //if right sensors(1, 2) read higher value, then the robot should turn right
+    if( (ORsensorVal_1 + ORsensorVal_2/2) > ((ORsensorVal_3 + ORsensorVal_4)/2 + 500) ){
+      Serial.println("In turningright");
+      turnRightForward_Fast(TURNING_SPEED_MAX, turningTime);
+      //Stop();
+    }else if( (ORsensorVal_1 + ORsensorVal_2)/2 > ((ORsensorVal_3 + ORsensorVal_4)/2 + 200) ){
+      turnRightForward_Slow(TURNING_SPEED_NORM, turningTime);
+    }
+    
+  //if left sensors(3, 4) read higher value, then the robot should turn left
+    if( (ORsensorVal_1 + ORsensorVal_2)/2 < ((ORsensorVal_3 + ORsensorVal_4)/2 - 500) ){
+      Serial.println("In turningleft");
+      turnLeftForward_Fast(TURNING_SPEED_MAX, turningTime);
+      //Stop();
+    }else if( (ORsensorVal_1 + ORsensorVal_2)/2 < ((ORsensorVal_3 + ORsensorVal_4)/2 - 200)){
+      turnLeftForward_Slow(TURNING_SPEED_NORM, turningTime);
+    }
+    
+  }else{
+    Serial.println("In Stop");
+
+    //boolean isBreakpoint = checkBreakpoint(1000);
+
+    Stop();
+    
+    /*if(isBreakpoint == false){
+      Stop();
+    }*/  
+    /*int numCycle = 0;
+    for(numCycle = 0; numCycle <= 100; numCycle++){
+      
+    }*/
+  }
+}
+
+//Preparing and calibrating
+void lineFollowPrepare(){
+  int refVal = 0;
+  int count = 0;
+  int i = 0;
+
+  //Calibrating the sensors, finding max and min reflectance values
+  for(count = 0; count < 10; count++){
+    for(i = 15; i < 20; i++){
+      refVal = analogRead(i);
+      if(refVal > maxSensorVal){
+        maxSensorVal = refVal;
+      }
+  
+      if(refVal < minSensorVal){
+        minSensorVal = refVal; 
+      }                               
+  
+      delay(1);
+    }
+  }
+
+  midVal = (maxSensorVal + minSensorVal)/2;
+}
+
+void scanGround(){
+  ORsensorVal_1 = analogRead(ORsensorpin_1);
+  //TempORsensorVal_2 = analogRead(ORsensorpin_2);
+  ORsensorVal_2 = analogRead(ORsensorpin_2);
+  //ORsensorVal_2 = map(TempORsensorVal_2, minSensorVal + 10, maxSensorVal - 100, minSensorVal, maxSensorVal);
+  ORsensorVal_3 = analogRead(ORsensorpin_3);
+  ORsensorVal_4 = analogRead(ORsensorpin_4);
+}
+
+
+
  
 
--- hall-effec_final/hall-effec_final.ino
+++ hall-effec_final/hall-effec_final.ino
@@ -1,25 +1,37 @@
 // read RPM
  volatile int half_revolutions = 0;
- int rps = 0;
+ int rpm = 0;
  unsigned long lastmillis = 0;
+double interval;
+
+
+
+const int HALL_EFFECT_PIN=2;
+ 
  String pole;
 const int E1 = 5;
 const int M1 = 4;
 const int E2 = 6;
 const int M2 = 7;
- 
+ volatile int record_time=0;
  unsigned long timeold;
  void setup(){
  Serial.begin(9600); 
  pinMode(2,INPUT_PULLUP);
  attachInterrupt(digitalPinToInterrupt(2), rpm_fan, FALLING);
 
-
    half_revolutions = 0;
-   rps = 0;
+   rpm = 0;
    timeold = 0;
  }
  void loop(){
+if (record_time==0) { 
+      timeold=millis();
+ }
+ else if(record_time==1){
+      interval=millis-timeold;
+ }
+  
    moveForward(255);
    if(digitalRead(2)==HIGH){
     pole="North";
@@ -28,29 +40,44 @@ const int M2 = 7;
    if(digitalRead(2)==LOW){
     pole="SOUTH";
   }
- if (half_revolutions >= 2) { 
+ 
      //Update RPM every 20 counts, increase this for better RPM resolution,
      //decrease for faster update
-    // rpm = 30*1000/(millis() - timeold)*half_revolutions;
-    double arc_time;
-    arc_time=half_revolutions/(millis()-timeold)*1000;
-    rps=arc_time*5;
+     rpm = 30*1000/(millis() - timeold)*half_revolutions;
+    
      
-    Serial.print("RPs =\t"); //print the word "RPM" and tab.
-    Serial.print(rps); // print the rpm value.
-   
+    Serial.print("RPM =\t"); //print the word "RPM" and tab.
+    Serial.print(rpm); // print the rpm value.
+    Serial.print("\t Hz=\t"); //print the word "Hz".
+    Serial.print(half_revolutions/2); //print revolutions per second or Hz. And print new line or enter
+    Serial.print("\t Pole:=\t");
+    Serial.println(pole);
+    
    
    timeold = millis();
-    half_revolutions = 0;
+     half_revolutions = 0;
    }
   
  }
  // this code will be executed every time the interrupt 0 (pin2) gets low.
  void rpm_fan(){
-  half_revolutions++;
+  //half_revolutions++;
+  if(record_time==0){
+    record_time=1;
+  }
+  else if(record_time==1){
+    record_time=0;
+  }
   Serial.println(half_revolutions);
 
  }
+
+
+void hall_effect(){
+  while(digitalRead
+  
+  
+}
 
  void moveForward(long power) {
   digitalWrite(M1, HIGH);
